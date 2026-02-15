24dcc484c7856edcf6f705eee205fd5cb0e978ea
owenhochwald <owenhochwald@gmail.com>
2026-02-15 00:28:39 -0800

    Add S3 backend configuration to Terraform

--- terraform.tf
+++ terraform.tf
@@ -6,5 +6,14 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket         = "carton-bucket-state-0002"
+    key            = "dev/egg-vault.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+
+
   required_version = ">= 1.2"
 }
