10bdd086f006766e3bed7babe6447229e1964e10
owenhochwald <owenhochwald@gmail.com>
2026-02-15 00:28:43 -0800

    Add DynamoDB table, KMS key, and S3 bucket resources to Terraform configuration

--- main.tf
+++ main.tf
@@ -13,11 +13,48 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+resource "aws_dynamodb_table" "egg_carton" {
+  name         = "EggCarton-Eggs"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "Owner"
+  range_key    = "SecretID"
+
+  attribute {
+    name = "Owner"
+    type = "S"
+  }
+
+  attribute {
+    name = "SecretID"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.vault_master.arn
+  }
 
   tags = {
-    Name = "Terraform Web Server"
+    Project = "EggCarton"
+  }
+}
+
+resource "aws_kms_key" "vault_master" {
+  description             = "Master key for encrypting secrets"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+}
+resource "aws_kms_alias" "vault_master_alias" {
+  name          = "alias/eggcarton-master"
+  target_key_id = aws_kms_key.vault_master.key_id
+}
+
+resource "aws_s3_bucket" "carton" {
+  bucket = "carton-storage-bucket"
+
+  tags = {
+    Name        = "Carton Storage Bucket"
+    Environment = "Dev"
   }
 }
