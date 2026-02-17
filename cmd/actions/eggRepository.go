ab01dd7e72513de7892ee0af3f668cf78c9684cf
owenhochwald <owenhochwald@gmail.com>
2026-02-17 15:35:51 -0800

    Implement GetAllEggs method in EggRepository and update handler to retrieve all eggs for a given owner

--- cmd/actions/eggRepository.go
+++ cmd/actions/eggRepository.go
@@ -12,6 +12,7 @@ import (
 
 type EggActions interface {
 	GetEgg(ctx context.Context, owner string) (Egg, error)
+	GetAllEggs(ctx context.Context, owner string) ([]Egg, error)
 	PutEgg(ctx context.Context, egg Egg) error
 	BreakEgg(ctx context.Context, owner, secretID string) error
 }
@@ -46,6 +47,33 @@ func (r EggRepository) GetEgg(ctx context.Context, owner string) (Egg, error) {
 		}
 	}
 	return egg, err
+}
+
+func (r EggRepository) GetAllEggs(ctx context.Context, owner string) ([]Egg, error) {
+	var eggs []Egg
+	params, err := attributevalue.MarshalList([]interface{}{owner})
+	if err != nil {
+		panic(err)
+	}
+	response, err := r.DynamoDbClient.ExecuteStatement(ctx, &dynamodb.ExecuteStatementInput{
+		Statement: aws.String(
+			fmt.Sprintf("SELECT * FROM \"%v\" WHERE Owner=?",
+				r.TableName)),
+		Parameters: params,
+	})
+	if err != nil {
+		log.Printf("Couldn't get eggs for %v. Here's why: %v\n", owner, err)
+		return eggs, err
+	}
+
+	// Unmarshal all items
+	err = attributevalue.UnmarshalListOfMaps(response.Items, &eggs)
+	if err != nil {
+		log.Printf("Couldn't unmarshal response. Here's why: %v\n", err)
+		return eggs, err
+	}
+
+	return eggs, nil
 }
 
 func (r EggRepository) PutEgg(ctx context.Context, egg Egg) error {
